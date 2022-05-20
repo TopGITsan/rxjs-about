672babb474099e95bf200f1944f60915dcf16d81
TopGitSan <c.toparcean@gmail.com>
2022-05-20 16:28:42 +0200

    refactor(multicast): use share operator to multicast a unicast observable

--- rx-operator/interval.js
+++ rx-operator/interval.js
@@ -1,11 +1,10 @@
 import { interval, Subject } from "rxjs";
-import { multicast, refCount, tap } from 'rxjs/operators';
+import {share, tap } from 'rxjs/operators';
 
 export const interval$ = interval(2000).pipe(
     tap((val) => console.log("new interval : ", val))
   );
 
 export const multicastedInterval$ =  interval$.pipe(
-  multicast(()=> new Subject()),
-  refCount()
+  share()
 )

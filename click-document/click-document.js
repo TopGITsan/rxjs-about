46b17256075e07694472793d4ce9729a83bd483d
TopGitSan <c.toparcean@gmail.com>
2022-05-27 12:13:11 +0200

    refactor(retry): add optional arguments using object destructuring

--- click-document/click-document.js
+++ click-document/click-document.js
@@ -1,4 +1,4 @@
-import { fromEvent, throwError, timer } from "rxjs";
+import { fromEvent, of, throwError, timer } from "rxjs";
 import { catchError, mergeMap, mergeMapTo, retryWhen } from "rxjs/operators";
 
 const click$ = fromEvent(document, "click");
@@ -6,27 +6,31 @@ const click$ = fromEvent(document, "click");
 export const clickError$ = click$.pipe(
   mergeMapTo(
     throwError({ status: 400, message: "Server error" }).pipe(
-      customRetry,
+      customRetry(),
       catchError((err) => of(err))
     )
   )
 );
 
-function customRetry(source) {
-  return source.pipe(
-    retryWhen((attempts) => {
-      return attempts.pipe(
-        mergeMap((error, i) => {
-          const attemptNumber = i + 1;
-          if (attemptNumber > 3 || [402, 500].find((e) => e == error.status)) {
-            console.log("Giving up");
-            return throwError(error);
-          }
-          console.log(`Attempt ${attemptNumber}: retrying in ${attemptNumber * 1000}ms`);
-          return timer(attemptNumber * 1000);
-        })
-      );
-    }),
-    catchError((err) => of(err))
-  );
+function customRetry(
+  {retryAttempts= 3, scalingDuration=1500, excludedStatusCodes = []}={}
+  ) {
+  return function (source) {
+    return source.pipe(
+      retryWhen((attempts) => {
+        return attempts.pipe(
+          mergeMap((error, i) => {
+            const attemptNumber = i + 1;
+            if (attemptNumber > retryAttempts || excludedStatusCodes.find((e) => e == error.status)) {
+              console.log("Giving up");
+              return throwError(error);
+            }
+            console.log(`Attempt ${attemptNumber}: retrying in ${attemptNumber * scalingDuration}ms`);
+            return timer(attemptNumber * scalingDuration);
+          })
+        );
+      }),
+      catchError((err) => of(err))
+    );
+  };
 }

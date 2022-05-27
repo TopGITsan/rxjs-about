1efb254487c7d2c2172696c7936be62389a3a9ba
TopGitSan <c.toparcean@gmail.com>
2022-05-27 12:03:49 +0200

    refactor(retry): extract functionaly

--- click-document/click-document.js
+++ click-document/click-document.js
@@ -6,20 +6,27 @@ const click$ = fromEvent(document, "click");
 export const clickError$ = click$.pipe(
   mergeMapTo(
     throwError({ status: 400, message: "Server error" }).pipe(
-      retryWhen((attempts) => {
-        return attempts.pipe(
-          mergeMap((error, i) => {
-            const attemptNumber = i + 1;
-            if (attemptNumber > 3 || [402, 500].find((e) => e == error.status)) {
-              console.log("Giving up");
-              return throwError(error);
-            }
-            console.log(`Attempt ${attemptNumber}: retrying in ${attemptNumber * 1000}ms`);
-            return timer(attemptNumber * 1000);
-          })
-        );
-      }),
+      customRetry,
       catchError((err) => of(err))
     )
   )
 );
+
+function customRetry(source) {
+  return source.pipe(
+    retryWhen((attempts) => {
+      return attempts.pipe(
+        mergeMap((error, i) => {
+          const attemptNumber = i + 1;
+          if (attemptNumber > 3 || [402, 500].find((e) => e == error.status)) {
+            console.log("Giving up");
+            return throwError(error);
+          }
+          console.log(`Attempt ${attemptNumber}: retrying in ${attemptNumber * 1000}ms`);
+          return timer(attemptNumber * 1000);
+        })
+      );
+    }),
+    catchError((err) => of(err))
+  );
+}

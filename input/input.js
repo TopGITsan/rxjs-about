6309666a2864e32be3ebffde5a747317dd275e11
TopGitSan <c.toparcean@gmail.com>
2022-07-07 13:05:23 +0200

    refactor(*): extract logic from the pipe operator into a function

--- input/input.js
+++ input/input.js
@@ -12,15 +12,23 @@ export const typeahead = document.getElementById("typeahead");
 
 const input$ = fromEvent(typeahead, "input");
 
+const fakeAjaxTypeahead = (param = 'rxjs') => (sourceObservable) => {
+  return sourceObservable.pipe(
+    debounceTime(200),
+    pluck("target", "value"),
+    distinctUntilChanged(),
+    switchMap((searchTerm) =>
+      from(["input", "top", "google", "goggle", "index", "indian", "do", "dit", "done", param]).pipe(
+        filter((s) => s.includes(searchTerm)),
+        catchError(() => EMPTY)
+      )
+    ),
+    catchError((err) => of(err))
+  );
+};
+
 export const typeahead$ = input$.pipe(
-  debounceTime(200),
-  pluck("target", "value"),
-  distinctUntilChanged(),
-  switchMap((searchTerm) =>
-    from(["input", "top", "google", "goggle", "index", "indian", "do", "dit", "done"]).pipe(
-      filter((s) => s.includes(searchTerm)),
-      catchError(()=> EMPTY)
-    )
-  ),
-  catchError((err) => of(err))
+  // functions that apear in the pipe method accept an observable and return a new observable
+  // will have our function return a function which accepts the source observable
+  fakeAjaxTypeahead()
 );
